4e08d162c6b776ee56532642b344bc7dfe04c205
Florence Jacobs <70036250+FlorenceJacobs@users.noreply.github.com>
2021-03-02 10:16:41 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <headerList></headerList>
+  </div>
+</template>
+
+<script>
+import HeaderList from './components/HeaderList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'headerList' : HeaderList
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #9cdcc4;
+}
+button {
+  background-color: #41b783 !important;
+}
+</style>
---
+++ src/components/HeaderList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- Ajoutez ici une balise header avec le texte souhaité -->
+    <!-- Ajoutez ci l'image logo_vue.png -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderList'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  header {
+    background-color: #34495e;
+    width: 100%;
+    min-height: 5rem;
+    text-align: center;
+    line-height: 5rem;
+    font-size: 1.5rem;
+    color : white;
+  }
+  img {
+    position : absolute;
+    top : 0px;
+  }
+</style>
